ed669c68c27a7adea9f151001b5d5f82c5dd6f5e
xMat27 <mathieu.belot34@bbox.fr>
2024-11-12 15:07:42 +0100

    modif placements

---
+++ train_yolo.py
@@ -0,0 +1,15 @@
+from ultralytics import YOLO
+
+# Charger le modèle YOLO pré-entrainé (par exemple YOLOv8s)
+model = YOLO("yolo11n.pt")  # Remplace "yolov8s.pt" par le modèle de ton choix
+
+# Chemin vers le fichier de configuration de ton dataset
+dataset_path = "dataset/data.yaml"  # Assure-toi que ce chemin est correct
+
+# Entraînement du modèle
+model.train(
+    data=dataset_path,
+    epochs=50,        # Nombre d'époques
+    batch=16,         # Taille du batch (ajuste selon la capacité de ta machine)
+    name="custom_yolo_model"  # Nom du dossier où seront enregistrés les résultats
+)
