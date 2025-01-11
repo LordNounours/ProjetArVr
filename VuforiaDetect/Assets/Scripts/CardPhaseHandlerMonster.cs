e603a4b673614bc5e99d1d58c87797fa563b587a
LordNounours <146192016+LordNounours@users.noreply.github.com>
2025-01-11 18:05:45 +0100

    bouton utilisable et var  current phase ou mettre la phase

---
+++ VuforiaDetect/Assets/Scripts/CardPhaseHandlerMonster.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class CardPhaseHandlerMonster : MonoBehaviour
+{
+
+    public List<int> usablePhases = new List<int>();
+
+    public int currentPhase;
+
+    private Renderer renderer;
+
+
+    void Start()
+    {
+        usablePhases.Add(2); //MP1
+        usablePhases.Add(4); //MP2
+        renderer = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+         renderer.material.color = IsCardUsable(currentPhase) ? Color.green : Color.red;
+    }
+
+    public bool IsCardUsable(int phase)
+    {
+        return usablePhases.Contains(phase);
+    }
+}
---
+++ VuforiaDetect/Assets/Scripts/CardPhaseHandlerTrap.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class CardPhaseHandlerTrap : MonoBehaviour
+{
+
+    public List<int> usablePhases = new List<int>();
+
+    public int currentPhase;
+
+    private Renderer renderer;
+
+    void Start()
+    {
+        usablePhases.Add(1); //SP
+        usablePhases.Add(2); //MP1
+        usablePhases.Add(3); //BP
+        usablePhases.Add(4); //MP2
+        usablePhases.Add(5); //EP
+        renderer = GetComponent<Renderer>();    }
+
+    void Update()
+    {
+         renderer.material.color = IsCardUsable(currentPhase) ? Color.green : Color.red;
+    }
+
+    public bool IsCardUsable(int phase)
+    {
+        return usablePhases.Contains(phase);
+    }
+}
