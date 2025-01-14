20c858a20f1058c9cc2644f1aec66e2cb17ac637
Patateon <maximebeldjilali@gmail.com>
2025-01-14 23:28:05 +0100

    Update to handle new input + fix resolution

--- VuforiaDetect/Assets/Scripts/Toggle3DHandler.cs
+++ VuforiaDetect/Assets/Scripts/Toggle3DHandler.cs
@@ -1,20 +1,61 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Toggle3DHandler : MonoBehaviour
 {
-    private bool isChecked = false; //Possession
+    private bool isChecked = false;
     private Renderer renderer;
+    private Camera mainCamera;
+
+    // Input Actions reference
+    private InputAction mouseClickAction;
+
+    void Awake()
+    {
+        // Create a new instance of the input action or link it via your Input Actions asset
+        var inputActions = new InputActionMap("Click");
+        mouseClickAction = inputActions.AddAction("MouseClick", binding: "<Mouse>/leftButton");
+
+        // Enable the input action
+        mouseClickAction.Enable();
+    }
+
+    void OnEnable()
+    {
+        mouseClickAction.performed += OnMouseClick;
+    }
+
+    void OnDisable()
+    {
+        mouseClickAction.performed -= OnMouseClick;
+        mouseClickAction.Disable();
+    }
 
     void Start()
     {
         renderer = GetComponent<Renderer>();
+        mainCamera = Camera.main;
         UpdateVisualState();
     }
 
-    void OnMouseDown()
+    private void OnMouseClick(InputAction.CallbackContext context)
     {
-        isChecked = !isChecked;
-        UpdateVisualState();
+        if (context.performed)
+        {
+            // Get mouse position from Pointer.current
+            Vector2 mousePosition = Pointer.current.position.ReadValue();
+            Ray ray = mainCamera.ScreenPointToRay(mousePosition);
+
+            // Perform raycast to check object hit
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                if (hit.collider.gameObject == gameObject)
+                {
+                    isChecked = !isChecked;
+                    UpdateVisualState();
+                }
+            }
+        }
     }
 
     private void UpdateVisualState()
